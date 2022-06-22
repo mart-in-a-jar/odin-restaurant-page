8018fafa1f4b88b5e13dec20fb2eb91c8883beec
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2022-06-22 12:38:45 +0200

    Set style on active menu button

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import { clearContent, renderTemplate, renderStartPage, changePage } from "./dra
 
 renderTemplate();
 
+
+// Page navigation
 const menuButtons = document.querySelectorAll("header ul.menu li a");
-
-
 menuButtons.forEach(button => {
     button.addEventListener("click", () => {
         changePage(button.textContent.toLowerCase());
+        
+        menuButtons.forEach(button => {
+            button.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
     });
 });
 
